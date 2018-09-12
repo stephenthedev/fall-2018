5c0ce1a59b10afd43c8d9ab60a04e3ed493079d8
Stephen Raghunath <raghunat@fredonia.edu>
2018-09-12 17:36:52 -0400

    Query Parameters

---
+++ 207/query-parameters/parameters.php
@@ -0,0 +1,13 @@
+<?php
+//query-parameters/parameters.php
+ ?>
+
+ <h1>Helllooooo Parameters</h1>
+
+ <?php
+
+$param1 = $_POST["param1"];
+$param2 = $_POST["param2"];
+
+echo "<b>$param1 $param2</b>";
+  ?>
