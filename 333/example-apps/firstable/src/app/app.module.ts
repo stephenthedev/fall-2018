7cf414dccbb42ee4a302d7bfc67c1c1b09e9c6f9
Stephen Raghunath <stephen.raghunath@acquia.com>
2018-09-21 14:47:05 -0400

    Added the page

--- 333/example-apps/firstable/src/app/app.module.ts
+++ 333/example-apps/firstable/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ItemPage } from '../pages/item/item';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ItemPage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { ListPage } from '../pages/list/list';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ItemPage
   ],
   providers: [
     StatusBar,
--- 333/example-apps/firstable/src/pages/list/list.ts
+++ 333/example-apps/firstable/src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ItemPage } from '../item/item';
 
 /**
  * Generated class for the ListPage page.
@@ -30,4 +31,14 @@ export class ListPage {
     this.items.push(item);
   }
 
+  removeItem(item: string) {
+    if (confirm("Are you sure you did this?")) {
+      let index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    }
+  }
+
+  goToItem(item: string) {
+    this.navCtrl.push(ItemPage, {item: item});
+  }
 }
