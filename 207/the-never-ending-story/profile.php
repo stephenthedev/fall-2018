2f23cd2a8821480b9ddcaa6a03706de236ebef80
Stephen Raghunath <stephen.raghunath@acquia.com>
2018-09-24 17:45:16 -0400

    Added base stuff for neverendingstory php 9/24

---
+++ 207/the-never-ending-story/profile.php
@@ -0,0 +1,71 @@
+<?php
+// profile.php
+
+require('header.php');
+
+require('navbar.php');
+ ?>
+
+<img id="profile-pic" src="https://free-wired.com/wp-content/uploads/2018/05/headshot-resume-awesome-the-national-resume-writers-association-certification-of-headshot-resume.jpg" alt="">
+
+<section id="profile-content">
+  <h2>Your Stories...</h2>
+  <a id="new-story" href="#">New Story</a>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Ch. #</th>
+        <th>Title</th>
+        <th># of Ch.s.</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+          <td>2</td>
+          <td>Some awesome story</td>
+          <td>67</td>
+          <td>
+            <a href="#">Continue Writing</a>
+          </td>
+      </tr>
+      <tr>
+          <td>2</td>
+          <td>Some awesome story</td>
+          <td>67</td>
+          <td>
+            <a href="#">Continue Writing</a>
+          </td>
+      </tr>
+      <tr>
+          <td>2</td>
+          <td>Some awesome story</td>
+          <td>67</td>
+          <td>
+            <a href="#">Continue Writing</a>
+          </td>
+      </tr>
+      <tr>
+          <td>2</td>
+          <td>Some awesome story</td>
+          <td>67</td>
+          <td>
+            <a href="#">Continue Writing</a>
+          </td>
+      </tr>
+      <tr>
+          <td>2</td>
+          <td>Some awesome story</td>
+          <td>67</td>
+          <td>
+            <a href="#">Continue Writing</a>
+          </td>
+      </tr>
+    </tbody>
+  </table>
+</section>
+
+
+ <?php require('footer.php') ?>
