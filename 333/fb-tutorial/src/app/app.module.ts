b8f0e9b768f0bb405c2f5c9a4ddd34c415a4c0b6
StotTot <a.debride.2016@2gmail.com>
2018-10-13 21:06:05 -0400

    Homework 2 completed

---
+++ 333/fb-tutorial/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { FbLoginPage} from '../pages/fb-login/fb-login';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    FbLoginPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    FbLoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Facebook,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
