1b738b6173136fbe64eae0151d6fb73f72c345e2
Stephen Raghunath <stephen.raghunath@acquia.com>
2018-10-24 17:34:03 -0400

    Updated

--- 207/the-never-ending-story/signup.php
+++ 207/the-never-ending-story/signup.php
@@ -7,19 +7,19 @@ require('navbar.php');
 
 <h1 id="signupTitle">The Never Ending Story</h1>
 
-<form id="signupForm">
-  <input type="file" name="pic">
+<form id="signupForm" action="create-user.php" method="POST">
+  <input type="file" name="img">
 
   <div>
-    <input type="text" placeholder="Username">
-    <input type="password" placeholder="Password">
-    <input type="password" placeholder="Confirm Password">
+    <input name="email" type="text" placeholder="Email">
+    <input name="password" type="password" placeholder="Password">
+    <input name="confirm_password" type="password" placeholder="Confirm Password">
   </div>
 
   <div>
-    <input type="text" placeholder="Name (Alias)">
-    <input type="text" placeholder="Email">
-    <input type="text" placeholder="Date of Birth">
+    <input name="first_name" type="text" placeholder="First Name">
+    <input name="last_name" type="text" placeholder="Last Name">
+    <input name="alias" type="text" placeholder="Alias">
   </div>
 
   <button>Register</button>
