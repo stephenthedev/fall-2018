a7d77393daa7285c0f41a08bad8149ca611b46ad
Stephen Raghunath <raghunat@fredonia.edu>
2018-11-07 21:07:05 -0500

    updated

--- 333/tnes-api/config/routes.rb
+++ 333/tnes-api/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :chapters
   resources :stories
 
-  get 'profile', do: 'stories#profile'
+  get 'profile', to: 'stories#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
